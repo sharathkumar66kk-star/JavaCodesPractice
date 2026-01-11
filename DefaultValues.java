3b0832811f29b21db1665a10e912d8038e3318d4
sharathkumar66kk-star <sharathkumar66kk@gmail.com>
2026-01-11 13:12:52 +0530

    Added new files

---
+++ DefaultValues.java
@@ -0,0 +1,30 @@
+public class DefaultValues {
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bool;
+    public static void main(String[] args) {
+        DefaultValues obj = new DefaultValues();
+        
+        System.out.println("Deafuault byte value :" + obj.b);
+        System.out.println("Deafuault short value :" + obj.s);
+        System.out.println("Deafuault int value :" + obj.i);
+        System.out.println("Deafuault float value :" + obj.f);
+        System.out.println("Deafuault double value :" + obj.d);
+        System.out.println("Deafuault char  value :" + obj.c);
+        System.out.println("Deafuault boolean value :" + obj.bool);
+        System.out.println("Deafuault long value :" + obj.l);
+        
+        
+
+
+
+
+
+        
+    }
+}
